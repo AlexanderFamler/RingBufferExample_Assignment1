2d584364e9423e0aa3a4ac2e996939f2bb330486
AlexanderFamler <famlerrr@yahoo.com>
2023-03-28 16:34:37 +0200

    more tests

--- src/test/java/at/jku/swtesting/RingBufferTest.java
+++ src/test/java/at/jku/swtesting/RingBufferTest.java
@@ -37,4 +37,43 @@ public class RingBufferTest {
 		assertEquals(2, a.size());
 	}
 
+	@Test
+	void testEnqueue() {
+		a.enqueue("a");
+		assertEquals("a", a.peek());
+		a.enqueue("b");
+		a.enqueue("c");
+		a.enqueue("d");
+		assertEquals("b", a.peek());
+		a.dequeue();
+		a.dequeue();
+		assertEquals("d", a.peek());
+	}
+
+	@Test
+	void testDequeue() {
+		a = null;
+		assertThrows(RuntimeException.class , () -> {a.dequeue();});
+
+		a = new RingBuffer<>(3);
+		a.enqueue("1");
+		assertEquals("1" , a.dequeue());
+		a.enqueue("2");
+		a.enqueue("3");
+		a.dequeue();
+		assertEquals("3" , a.dequeue());
+	}
+
+	@Test
+	void testPeek() {
+		a = null;
+		assertThrows(RuntimeException.class , () -> {a.peek();});
+
+		a = new RingBuffer<>(3);
+		a.enqueue("a");
+		assertEquals("a", a.peek());
+		a.enqueue("b");
+		assertNotEquals("b", a.peek());
+	}
+
 }
