0a911262018a9a9a7bf7ca58590a7eaf2724945e
Philip Duong <philip.duong@hotmail.com>
2023-03-29 17:21:37 +0200

    Standardized test names, simplified tests, added TODOs

--- src/test/java/at/jku/swtesting/RingBufferTest.java
+++ src/test/java/at/jku/swtesting/RingBufferTest.java
@@ -4,76 +4,113 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+Note: follow naming convention: If_Condition_WhatShouldHappen()
+TODO: or think of a better one because this one might actually be too confusing
+TODO: tests have not been validated yet
+
+TODOs:
+- standardize comments
+- maybe only use one method per unit test (so you know it is *this one* method that fails)
+ */
+
 public class RingBufferTest {
 
-	RingBuffer<String> a;
+	RingBuffer<String> ringbuffer;
 	@BeforeEach
 	void setUp() {
-		a = new RingBuffer<>(3);
+		ringbuffer = new RingBuffer<>(3);
 	}
 
-	//test for constructor
-	@Test
-	void testRingBufferConstructor() {
-		assertNotNull(a);
-		assertThrows(IllegalArgumentException.class, () -> { new RingBuffer<>(0);});
+	//Test
+	void IfCapacityOverZero_RingbufferInstantiated(){
+		assertNotNull(ringbuffer);
 	}
 
-	//test for capacity method
 	@Test
-	void testCapacity(){
-		assertEquals(3, a.capacity());
-		a.enqueue("one");
-		a.enqueue("two");
-		a.enqueue("three");
+	void IfCapacityUnderZero_constructorThrowsException() {
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> { new RingBuffer<>(0);});
+		String expectedMessage = "Initial capacity is less than one";
+		String actualMessage = exception.getMessage();
+		assertTrue(actualMessage.contains(expectedMessage));
+	}
+
+	//Test 2: tests capacity method
+	//TODO: too broad? does not test a specific funcationality
+	@Test
+	void IfThreeItemsEnqueued_ringBufferHasThreeItems(){
+		assertEquals(3, ringbuffer.capacity());
+		ringbuffer.enqueue("one");
+		ringbuffer.enqueue("two");
+		ringbuffer.enqueue("three");
 		//a.enqueue("four"); //works either way because of the wrap-around
-		assertEquals(a.size(), a.capacity());
+		assertEquals(ringbuffer.size(), ringbuffer.capacity());
 	}
 
+	//Test 3: tests size method
 	@Test
-	void testSize(){
-		a.enqueue("eins");
-		a.enqueue("zwei");
-		assertEquals(2, a.size());
+	void IfItemsEnqueued_sizeEqualsNoOfEnqueuedItems(){
+		ringbuffer.enqueue("eins");
+		assertEquals(1, ringbuffer.size());
+		ringbuffer.enqueue("zwei");
+		ringbuffer.enqueue("drei");
+		assertEquals(3, ringbuffer.size());
 	}
 
+	//Test 4: tests enqueue method
 	@Test
 	void testEnqueue() {
-		a.enqueue("a");
-		assertEquals("a", a.peek());
-		a.enqueue("b");
-		a.enqueue("c");
-		a.enqueue("d");
-		assertEquals("b", a.peek());
-		a.dequeue();
-		a.dequeue();
-		assertEquals("d", a.peek());
+		ringbuffer.enqueue("a");
+		assertEquals("a", ringbuffer.peek());
+		ringbuffer.enqueue("b");
+		ringbuffer.enqueue("c");
+		ringbuffer.enqueue("d");
+		assertEquals("b", ringbuffer.peek());
+		ringbuffer.dequeue();
+		ringbuffer.dequeue();
+		assertEquals("d", ringbuffer.peek());
 	}
 
+	//Test 5: tests dequeue method
 	@Test
-	void testDequeue() {
-		a = null;
-		assertThrows(RuntimeException.class , () -> {a.dequeue();});
+	void testDequeueException() {
+		ringbuffer = null;
+		Exception exception = assertThrows(RuntimeException.class , () -> {
+			ringbuffer.dequeue();});
 
-		a = new RingBuffer<>(3);
-		a.enqueue("1");
-		assertEquals("1" , a.dequeue());
-		a.enqueue("2");
-		a.enqueue("3");
-		a.dequeue();
-		assertEquals("3" , a.dequeue());
+		String expectedMessage = "Empty ring buffer.";
+		String actualMessage = exception.getMessage();
+		assertTrue(actualMessage.contains(expectedMessage));
 	}
 
-	@Test
-	void testPeek() {
-		a = null;
-		assertThrows(RuntimeException.class , () -> {a.peek();});
+	//Test
+	void testDequeueValue(){
+		ringbuffer.enqueue("1");
+		assertEquals("1" , ringbuffer.dequeue());
+		ringbuffer.enqueue("2");
+		ringbuffer.enqueue("3");
+		ringbuffer.dequeue();
+		assertEquals("3" , ringbuffer.dequeue());
+	}
 
-		a = new RingBuffer<>(3);
-		a.enqueue("a");
-		assertEquals("a", a.peek());
-		a.enqueue("b");
-		assertNotEquals("b", a.peek());
+
+	//Test 6: tests peek method
+	@Test
+	void testPeekException() {
+		Exception exception = assertThrows(RuntimeException.class , () -> {
+			ringbuffer.peek();
+		});
+		String expectedMessage = "Empty ring buffer.";
+		String actualMessage = exception.getMessage();
+		assertTrue(actualMessage.contains(expectedMessage));
+	}
+
+	//Test:
+	void testPeekValue(){
+		ringbuffer.enqueue("a");
+		assertEquals("a", ringbuffer.peek());
+		ringbuffer.enqueue("b");
+		assertNotEquals("b", ringbuffer.peek());
 	}
 
 }
